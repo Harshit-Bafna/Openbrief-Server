6c894da0baa4565893cfd099a03a7e1309256459
Harshit-Bafna <bafnaharshit7891@gmail.com>
2025-07-27 22:23:37 +0530

    fix: Routing

--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
 import applicationRouter from '../router/application'
+import authRouter from '../router/auth'
+import passwordRouter from '../router/password'
 
 const router = Router()
 
 router.use('/', applicationRouter)
+router.use('/auth', authRouter)
+router.use('/password', passwordRouter)
 
 export default router
